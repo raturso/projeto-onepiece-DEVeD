5efbeedf2738ddd494bd6a0242be4a27662aaa2b
Divino Morais <106784430+raturso@users.noreply.github.com>
2023-10-01 02:47:13 -0300

    Projeto OPDeD

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll(".personagem");
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao();
+
+        botao.classList.add("selecionado");
+
+        DesselecionarPersonagem();
+
+
+        personagens[indice].classList.add("selecionado");
+    });
+
+});
+function DesselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
